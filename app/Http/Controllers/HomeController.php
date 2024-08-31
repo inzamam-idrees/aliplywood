12bef531bf904a3edc60cfcacad3d7ba77a669f5
Inzamam Idrees <inzamam.idrees@codeinformatics.com>
2024-08-31 18:52:06 +0500

    Format

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,103 +41,103 @@ class HomeController extends Controller
     // }
 
     public function index()
-{
-    $totalProducts = Product::count();
-    $totalCategories = Category::count();
-    // $totalSales = Sale::count();
-    $totalSales = OrderDetails::count();
-    $totalSuppliers = Supplier::count();
-    $totalCustomers = Customer::count();
-    // $totalInvoices = Invoice::count();
-    $totalInvoices = Order::count();
-    $totalPurchases = Purchase::count();
+    {
+        $totalProducts = Product::count();
+        $totalCategories = Category::count();
+        // $totalSales = Sale::count();
+        $totalSales = OrderDetails::count();
+        $totalSuppliers = Supplier::count();
+        $totalCustomers = Customer::count();
+        // $totalInvoices = Invoice::count();
+        $totalInvoices = Order::count();
+        $totalPurchases = Purchase::count();
 
-    // Fetch monthly sales data from the sales table
-    // $monthlySales = Sale::selectRaw('SUM(amount) as total_amount, MONTH(created_at) as month')
-    //     ->groupBy(DB::raw('MONTH(created_at)'))
-    //     ->get();
-    $monthlySales = OrderDetails::selectRaw('SUM(total) as total_amount, MONTH(created_at) as month')
-        ->groupBy(DB::raw('MONTH(created_at)'))
-        ->get();
-
-
-    $formattedMonthlySales = [];
-    foreach ($monthlySales as $sale) {
-        $formattedMonthlySales[] = [
-            'month' => \DateTime::createFromFormat('!m', $sale->month)->format('F'), // Format month name
-            'total_amount' => (int) $sale->total_amount // Ensure the amount is an integer
-        ];
-    }
+        // Fetch monthly sales data from the sales table
+        // $monthlySales = Sale::selectRaw('SUM(amount) as total_amount, MONTH(created_at) as month')
+        //     ->groupBy(DB::raw('MONTH(created_at)'))
+        //     ->get();
+        $monthlySales = OrderDetails::selectRaw('SUM(total) as total_amount, MONTH(created_at) as month')
+            ->groupBy(DB::raw('MONTH(created_at)'))
+            ->get();
 
 
-    // $topSales = Sale::select('product_id', DB::raw('SUM(amount) as total_sales'))
-    //     ->groupBy('product_id')
-    //     ->orderByDesc('total_sales')
-    //     ->take(5)
-    //     ->get();
-    $topSales = OrderDetails::select('product_id', DB::raw('SUM(total) as total_sales'))
-        ->groupBy('product_id')
-        ->orderByDesc('total_sales')
-        ->take(5)
-        ->get();
-
-    $formattedTopSales = [];
-    foreach ($topSales as $sale) {
-        $product = Product::find($sale->product_id);
-        if ($product) {
-            $formattedTopSales[] = [
-                'productName' => $product->name,
-                'totalSales' => $sale->total_sales,
+        $formattedMonthlySales = [];
+        foreach ($monthlySales as $sale) {
+            $formattedMonthlySales[] = [
+                'month' => \DateTime::createFromFormat('!m', $sale->month)->format('F'), // Format month name
+                'total_amount' => (int) $sale->total_amount // Ensure the amount is an integer
             ];
         }
+
+
+        // $topSales = Sale::select('product_id', DB::raw('SUM(amount) as total_sales'))
+        //     ->groupBy('product_id')
+        //     ->orderByDesc('total_sales')
+        //     ->take(5)
+        //     ->get();
+        $topSales = OrderDetails::select('product_id', DB::raw('SUM(total) as total_sales'))
+            ->groupBy('product_id')
+            ->orderByDesc('total_sales')
+            ->take(5)
+            ->get();
+
+        $formattedTopSales = [];
+        foreach ($topSales as $sale) {
+            $product = Product::find($sale->product_id);
+            if ($product) {
+                $formattedTopSales[] = [
+                    'productName' => $product->name,
+                    'totalSales' => $sale->total_sales,
+                ];
+            }
+        }
+
+        // Get today's date and yesterday's date
+        $today = Carbon::today()->toDateString();
+        $yesterday = Carbon::yesterday()->toDateString();
+        $currentMonth = Carbon::now()->month;
+        $currentYear = Carbon::now()->year;
+
+        // Query sales data for today and yesterday
+        // $todaySales = Sale::whereDate('created_at', $today)->sum('amount');
+        // $yesterdaySales = Sale::whereDate('created_at', $yesterday)->sum('amount');
+        $todaySales = OrderDetails::whereDate('created_at', $today)->sum('total');
+        $yesterdaySales = OrderDetails::whereDate('created_at', $yesterday)->sum('total');
+        $monthlySalesPrice = OrderDetails::whereYear('created_at', $currentYear)
+                                    ->whereMonth('created_at', $currentMonth)
+                                    ->sum('total');
+        $totalSalesPrice = OrderDetails::sum('total');
+
+        // Fetch this week's sales
+        // $thisWeekSales = Sale::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
+        //                     ->sum('amount');
+        $thisWeekSales = OrderDetails::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
+                            ->sum('total');
+
+        // Fetch last week's sales
+        // $lastWeekSales = Sale::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])
+        //                     ->sum('amount');
+        $lastWeekSales = OrderDetails::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])
+                            ->sum('total');
+
+        return view('home', [
+            'monthlySales' => $formattedMonthlySales,
+            'formattedTopSales'=> $formattedTopSales,
+            'totalProducts' => $totalProducts,
+            'totalCategories' => $totalCategories,
+            'totalCustomers' => $totalCustomers,
+            'totalPurchases' => $totalPurchases,
+            'totalSales' => $totalSales,
+            'totalSuppliers' => $totalSuppliers,
+            'totalInvoices' => $totalInvoices,
+            'todaySales' => $todaySales, 
+            'yesterdaySales' => $yesterdaySales,
+            'monthlySalesPrice' => $monthlySalesPrice,
+            'totalSalesPrice' => $totalSalesPrice,
+            'thisWeekSales' =>$thisWeekSales,
+            'lastWeekSales' =>$lastWeekSales,
+        ]);
     }
-
-    // Get today's date and yesterday's date
-    $today = Carbon::today()->toDateString();
-    $yesterday = Carbon::yesterday()->toDateString();
-    $currentMonth = Carbon::now()->month;
-    $currentYear = Carbon::now()->year;
-
-    // Query sales data for today and yesterday
-    // $todaySales = Sale::whereDate('created_at', $today)->sum('amount');
-    // $yesterdaySales = Sale::whereDate('created_at', $yesterday)->sum('amount');
-    $todaySales = OrderDetails::whereDate('created_at', $today)->sum('total');
-    $yesterdaySales = OrderDetails::whereDate('created_at', $yesterday)->sum('total');
-    $monthlySalesPrice = OrderDetails::whereYear('created_at', $currentYear)
-                                ->whereMonth('created_at', $currentMonth)
-                                ->sum('total');
-    $totalSalesPrice = OrderDetails::sum('total');
-
-    // Fetch this week's sales
-    // $thisWeekSales = Sale::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
-    //                     ->sum('amount');
-    $thisWeekSales = OrderDetails::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
-                        ->sum('total');
-
-    // Fetch last week's sales
-    // $lastWeekSales = Sale::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])
-    //                     ->sum('amount');
-    $lastWeekSales = OrderDetails::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])
-                        ->sum('total');
-
-    return view('home', [
-        'monthlySales' => $formattedMonthlySales,
-        'formattedTopSales'=> $formattedTopSales,
-        'totalProducts' => $totalProducts,
-        'totalCategories' => $totalCategories,
-        'totalCustomers' => $totalCustomers,
-        'totalPurchases' => $totalPurchases,
-        'totalSales' => $totalSales,
-        'totalSuppliers' => $totalSuppliers,
-        'totalInvoices' => $totalInvoices,
-        'todaySales' => $todaySales, 
-        'yesterdaySales' => $yesterdaySales,
-        'monthlySalesPrice' => $monthlySalesPrice,
-        'totalSalesPrice' => $totalSalesPrice,
-        'thisWeekSales' =>$thisWeekSales,
-        'lastWeekSales' =>$lastWeekSales,
-    ]);
-}
 
     public function edit_profile(){
          return view('profile.edit_profile');
